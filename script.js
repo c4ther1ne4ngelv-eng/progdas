ef5a86b364ab7ca45995b18957c46ea0a1479dc8
c4ther1ne4ngelv-eng <c4ther1ne.4ngelv@gmail.com>
2025-11-13 15:02:32 +0700

    yes

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const learnBtn = document.querySelector('.btn[href="#testimoni"]');
-if (learnBtn) {
-    learnBtn.addEventListener('click', e => {
+
+if (learnBtn){
+    learnBtn.addEventListener("click", e => {
         e.preventDefault();
-        const target = document.querySelector('#testimoni');
-        if (target) {
-            target.scrollIntoView({ behavior: 'smooth' });
+        const target = document.querySelector("#testimoni");
+        if (target){
+            target.scrollIntoView({behavior: 'smooth'});
         }
     })
 }
 
-// notif stlh submit
-const form = document.querySelector('.contact-form'); //queryselector utk ambil data
-if(form){
-    form.addEventListener('submit', e => {
+const form = document.querySelector('.contact-form');
+if (form){
+    form.addEventListener("submit", e => {
         e.preventDefault();
-        alert('pesan berhasil dikirim');
+        alert("Form submitted!");
         form.reset();
     })
 }
 
 
-
-
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+window.onscroll =  () => {
+    if (document.documentElement.scrollTop > 200) {
+        scrollTopBtn.style.display = "block" ;
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+}
+scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({top :0, behavior: "smooth"});
+});
